97ba32173e2aafdfa5ac53ff707514bba7750b92
Tanja <tatjanatanaskovich@gmail.com>
2018-08-23 13:39:45 +0200

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,7 +57,17 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
-  
+
+  def alerts
+    alert = (flash[:alert]) || (flash[:error]) || (flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Tatjana Tanaskovic Portfolio", sticky: false, time: 4000)
+  end
 end
 
 
