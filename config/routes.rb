9e0bdbd65641065cd634d1c095586e35e0c3854a
Tanja <tatjanatanaskovich@gmail.com>
2018-09-14 11:57:31 +0200

    Created comment broadcast, configured the websocket route, and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'react-items', to: 'portfolios#react'
@@ -13,6 +12,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
 end
